da088bce5e5179f2793faadc12ff36283222a9a6
Andrew Pham <40774904+andrewvpham@users.noreply.github.com>
2022-10-13 18:06:09 -0700

    Added appdrawer and routing

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import { AppBar, HStack, IconButton } from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
-
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeRouter, Route, Link, Routes} from "react-router-native";
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Drawer } from 'react-native-material-drawer';
+import { Button } from "@react-native-material/core";
+import { Divider } from "@react-native-material/core";
+import { ListItem } from "@react-native-material/core";
 
 import Navbar from './navbar';
 import { SafeAreaView, StatusBar  } from 'react-native';
@@ -11,21 +15,33 @@ import { SafeAreaView, StatusBar  } from 'react-native';
 export default function App() {
   const [value, setValue] = React.useState('recents');
   const [appTitle, setAppTitle] = React.useState('Home');
+  const [drawerState, setDrawerState] = React.useState(false);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const switchState = () => {
+    if(drawerState==false) {
+      setDrawerState(true)
+    }
+    else {
+      setDrawerState(false)
+    }
+  }
+
   return (
-    
-    <SafeAreaView style={styles.container}>
+    <NativeRouter>
+      <SafeAreaView style={styles.container}>
+
       <AppBar
+      color="#4ed970"
       style={styles.appbar}
-    title={appTitle}
-    subtitle="Lorem ipsum"
-    centerTitle={true}
-    leading={props => (
-      <IconButton icon={props => <Icon name="menu" {...props} />} {...props} />
+      title={appTitle}
+      subtitle="Lorem ipsum"
+      centerTitle={true}
+      leading={props => (
+      <IconButton onPress={switchState} icon={props => <Icon name="menu" {...props} />} {...props} />
     )}
     trailing={props => (
       <IconButton
@@ -35,9 +51,63 @@ export default function App() {
     )}>
       
     </AppBar>
-    <Navbar />
+    <Drawer
+        open={drawerState}
+        drawerContent={
+        <View>
+          <Text>Drawer Content</Text>
+          <ListItem
+      title="Inbox"
+      leading={<Icon name="inbox" size={24} />}
+      trailing={props => <Icon name="chevron-right" {...props} />}
+    />
+    <ListItem
+      title="Drafts"
+      leading={<Icon name="email-open" size={24} />}
+      trailing={props => <Icon name="chevron-right" {...props} />}
+    />
+    <ListItem
+      title="Trash"
+      trailing={props => <Icon name="chevron-right" {...props} />}
+    />
+    <ListItem
+      title="Spam"
+      trailing={props => <Icon name="chevron-right" {...props} />}
+    />
+        </View>
+          
+        }
+        onClose={switchState}
+        animationTime={250}>
+        <View style={styles.body}>
+            <View style={{ marginTop: 20, alignItems: 'center', width: '100%', flex: 1 }}>
+            <Text style={{ marginBottom: 20 }}>
+              This is a page
+            </Text>
+            <TouchableOpacity onTouch={switchState}>
+            <Routes>
+      <Route path="/" element={<Navbar />} />
+      </Routes>
+              <Text>Toggle</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Drawer>
+
+    <Button title="Open Drawer" onPress={switchState}/>
+      <Link
+          to="/"
+          underlayColor="#ff0000"
+          style={styles.navItem}
+        >
+          <Text>About</Text>
+        </Link>
+
+     
       
     </SafeAreaView>
+    
+    </NativeRouter>
     
   );
 }
